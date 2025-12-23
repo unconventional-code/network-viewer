513b0cc6335f656360e65b2c8005a65ef6eaa7e4
Jackson Graves <jackson@unconventionalcode.com>
2025-12-23 04:18:11 -0600

    fix: simplify tabs hierarchy

--- src/Containers/ReqDetailContainer.tsx
+++ src/Containers/ReqDetailContainer.tsx
@@ -1,17 +1,41 @@
+import { useState } from "react";
 import { useNetwork } from "../state/network/Context";
-import { Tabs } from "../Components/Common/Tabs";
-import { Tab } from "../Components/Common/Tab";
 import { Headers } from "../Components/ReqDetail/Headers";
 import { IconCloseSign } from "../icons/IconCloseSign";
 import { Response } from "../Components/ReqDetail/Response";
 import { Request } from "../Components/ReqDetail/Request";
+import classNames from "classnames";
+
+type TabKey = "headers" | "request" | "response";
 
 export function ReqDetailContainer() {
   const { actions, state } = useNetwork();
   const reqDetail = state.reqDetail;
+  const [activeTab, setActiveTab] = useState<TabKey>("headers");
+
   const handleCloseClick = () => {
     actions.selectRequest(null);
   };
+
+  const tabs: { key: TabKey; name: string; component: React.ReactNode }[] = [
+    {
+      key: "headers",
+      name: "Headers",
+      component: <Headers data={reqDetail} />,
+    },
+    {
+      key: "request",
+      name: "Request",
+      component: <Request data={reqDetail} />,
+    },
+    {
+      key: "response",
+      name: "Response",
+      component: <Response data={reqDetail} />,
+    },
+  ];
+
+  const activeTabContent = tabs.find((tab) => tab.key === activeTab)?.component;
 
   return (
     <div
@@ -29,23 +53,39 @@ export function ReqDetailContainer() {
       >
         <IconCloseSign className="w-5 h-5 fill-brand-primary-gray" />
       </button>
-      <Tabs
-        activeClassName="text-brand-primary-dark-gray border-brand-blue"
-        defaultSelectedKey="headers"
-        navLinkClassName="px-m py-s text-h5 font-normal text-brand-primary-gray no-underline cursor-pointer border-b-2 border-transparent hover:text-brand-primary-dark-gray"
-        navTabsClassName="flex border-b border-border-color"
-        tabsContainerClassName="flex-1 overflow-auto"
+      <nav
+        id="tabs-nav"
+        data-testid="tabs-nav"
+        className="flex border-b border-border-color"
       >
-        <Tab eventKey="headers" name="Headers">
-          <Headers data={reqDetail} />
-        </Tab>
-        <Tab eventKey="request" name="Request">
-          <Request data={reqDetail} />
-        </Tab>
-        <Tab eventKey="response" name="Response">
-          <Response data={reqDetail} />
-        </Tab>
-      </Tabs>
+        {tabs.map((tab, index) => (
+          <a
+            key={tab.key}
+            id={`tab-${tab.key}`}
+            data-testid={`tab-${tab.key}`}
+            data-active={activeTab === tab.key}
+            className={classNames(
+              "px-m py-s text-h5 font-normal text-brand-primary-gray no-underline cursor-pointer border-b-2 border-transparent hover:text-brand-primary-dark-gray",
+              {
+                "text-brand-primary-dark-gray border-brand-blue":
+                  activeTab === tab.key,
+              }
+            )}
+            onClick={() => setActiveTab(tab.key)}
+            role="tab"
+            tabIndex={index}
+          >
+            {tab.name}
+          </a>
+        ))}
+      </nav>
+      <section
+        id="tabs-content"
+        data-testid="tabs-content"
+        className="flex-1 overflow-auto"
+      >
+        {activeTabContent}
+      </section>
     </div>
   );
 }
